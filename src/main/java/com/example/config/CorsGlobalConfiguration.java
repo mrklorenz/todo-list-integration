93074b74c9f7ac52b68a37494409f73b30a278ae
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-19 14:37:58 +0800

    [CORS][Mark] feat: Add POST for allowed methods

--- src/main/java/com/example/config/CorsGlobalConfiguration.java
+++ src/main/java/com/example/config/CorsGlobalConfiguration.java
@@ -15,7 +15,7 @@ public class CorsGlobalConfiguration {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET");
+                        .allowedMethods("GET" ,"POST");
             }
         };
     }
