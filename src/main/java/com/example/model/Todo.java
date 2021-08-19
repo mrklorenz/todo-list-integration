0d752015d6f2088708f8944c9ba90e154d6676ca
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-19 13:34:35 +0800

    [CORS][Mark] feat: Add todo entity/model class

---
+++ src/main/java/com/example/model/Todo.java
@@ -0,0 +1,46 @@
+package com.example.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String text;
+    private boolean done;
+
+    private Todo(int id, String text, boolean done){
+        this.id = id;
+        this.text = text;
+        this.done = done;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+}
